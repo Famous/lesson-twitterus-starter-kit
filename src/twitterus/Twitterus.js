30a914b6402584e5930c6118576f6a0908bd4094
Morgan Plant <morgantheplant@Morgans-MacBook-Pro.local>
2015-04-23 15:47:21 -0700

    add first step

--- src/twitterus/Twitterus.js
+++ src/twitterus/Twitterus.js
@@ -2,12 +2,13 @@ var FamousPlatform = require('famous');
 var Famous = FamousPlatform.core.Famous;
 
 function Twitterus(mount) {
-    //attach app to the DOM
     this.context = Famous.createContext(mount || 'body');
-    //create the root node of the app
     this.root = this.context.addChild();
     
-    //call .addChild() on 'this.root' to add a child node
+    //add three child nodes extending frmo the root node
+    var headerNode = this.root.addChild()
+    var swapperNode = this.root.addChild()
+    var footerNode = this.root.addChild()
 }
 
 module.exports = Twitterus;
