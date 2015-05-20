e35888d5b924e3dc38d935e551c79ef3480932ab
DnMllr <me@dnmllr.io>
2015-05-19 17:52:36 -0700

    updated step-2

--- src/twitterus/Twitterus.js
+++ src/twitterus/Twitterus.js
@@ -3,9 +3,51 @@ var Node = require('famous/core/Node');
 function Twitterus(mount) {
     // Extend Node
     Node.call(this);
+
+    makeHeader(this);
+    makeFooter(this);
+    makeSwapper(this);
 }
 
 // Extend the prototype
 Twitterus.prototype = Object.create(Node.prototype);
+
+function makeHeader(node) {
+    // the header will be positioned defaultly
+    // along the top of its parent.
+    // It will be the complete width of its parent
+    // and 100 pixels tall.
+    node.addChild()
+        .setSizeMode('default', 'absolute')
+        .setAbsoluteSize(null, 100)
+        .addChild();
+}
+
+function makeSwapper(node) {
+    // the swapper will be 200 pixels smaller than
+    // its parent in Y and otherwise the same size.
+    // It will be position 100 pixels below its parent
+    // such that it clears the header
+    node.addChild()
+        .setDifferentialSize(null, -200)
+        .setPosition(0, 100)
+        .addChild();
+}
+
+function makeFooter(node) {
+    // the footer will be aligned
+    // to the bottom of its parent.
+    // Like the header it will be
+    // 100px tall and the complete width.
+    // note how we use MountPoint and Align
+    // together to line up the bottom of the footer
+    // with the bottom of the parent
+    node.addChild()
+        .setSizeMode('default', 'absolute')
+        .setAbsoluteSize(null, 100)
+        .setMountPoint(0, 1)
+        .setAlign(0, 1)
+        .addChild();
+}
 
 module.exports = Twitterus;
