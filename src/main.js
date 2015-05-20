0823b2d0950196c1c623a69c1b23c201755741a9
DnMllr <me@dnmllr.io>
2015-05-19 18:11:17 -0700

    boilerplate

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 'use strict';
 
 var FamousPlatform = require('famous');
-var Compositor = FamousPlatform.renderers.Compositor;
-var ThreadManager = FamousPlatform.renderers.ThreadManager;
-var Engine = FamousPlatform.engine;
-var Famous = FamousPlatform.core.Famous;
+var FamousEngine = FamousPlatform.core.FamousEngine;
 
 // Boilerplate
-var compositor = new Compositor();
-var threadmanger = new ThreadManager(Famous, compositor);
-var engine = new Engine();
-engine.update(threadmanger);
+FamousEngine.init();
 
 // App Code
-require('./twitterus/main')
+require('./twitterus/main');
+
