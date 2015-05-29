3e41f91621cbb4dda5aa44764f8fa21e715cb7d7
Morgan Plant <morgan@famo.us>
2015-05-29 12:35:33 -0700

    rename files

--- src/twitterus/index.js
+++ src/twitterus/index.js
@@ -1,5 +1,6 @@
 var Twitterus = require('./Twitterus');
 var FamousEngine = require('famous/core/FamousEngine');
- 
- //create the app and pass in the target element
+//start the Engine
+FamousEngine.init();
+//create the app and pass in the target element
 var twitterus = FamousEngine.createScene().addChild(new Twitterus());
